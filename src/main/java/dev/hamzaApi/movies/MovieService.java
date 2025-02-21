6018e9f2ee23b9cec2762c718570f16793bbbc83
Hamza Memon <hmemon@uoguelph.ca>
2025-02-21 17:12:48 -0500

    searching movie by ID

---
+++ src/main/java/dev/hamzaApi/movies/MovieService.java
@@ -0,0 +1,25 @@
+package dev.hamzaApi.movies;
+
+// uses repo class to talks to database and gets list of movies
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired // lets the framework know that we want to initialize moveRepo
+    private MovieRepository movieRepository;
+
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(ObjectId id) { // we say optional in case movie is NULL
+        return movieRepository.findById(id);
+    }
+}
