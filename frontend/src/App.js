4d085d1ef6176271713440fb007d28dcdde22ed6
Hamza Memon <hmemon@uoguelph.ca>
2025-02-21 23:46:34 -0500

    frontend improvements

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,16 @@ import { useState } from 'react';
 import Layout from './components/Layout';
 import {Routes, Route} from 'react-router-dom';
 import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
+import NotFound from './components/notFound/NotFound';
 
 function App() {
 
-  const [movies, setMovies] = useState([]); 
+  const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
 
   // handles https get request to an endpoint that returns an array of movie data 
   const getMovies = async () => {
@@ -29,6 +35,26 @@ function App() {
 
     }
   }
+  const getMovieData = async (movieId) => {
+     
+    try 
+    {
+        const response = await api.get(`/api/v1/movies/${movieId}`);
+
+        const singleMovie = response.data;
+
+        setMovie(singleMovie);
+
+        setReviews(singleMovie.reviews);
+        
+
+    } 
+    catch (error) 
+    {
+      console.error(error);
+    }
+
+  }
 
   // getMovies function exceuted when the app first loads
   useEffect(() => {
@@ -37,10 +63,13 @@ function App() {
 
   return (
     <div className="App">
+      <Header/>
       <Routes>
         <Route path="/" element={<Layout/>}>
             <Route path="/" element={<Home movies={movies}/>}></Route>
-            
+            <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+            <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
+            <Route path="*" element = {<NotFound/>}></Route>
         </Route>
       </Routes>
     </div>
